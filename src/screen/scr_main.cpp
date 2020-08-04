74239d3e6eb3e24ca1a9a8819adba263dd08fb9a
Tisego Monareng <tmonareng99@gmail.com>
2020-08-04 04:01:24 +0200

    4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -7,26 +7,21 @@ using namespace std;
 int main()
 {
 	auto myScreen = Screen{6,6};
-	myScreen.forward();
-	myScreen.set('*');
-	myScreen.down();
-	myScreen.set('*');
-	myScreen.move(3,3);
-	myScreen.set("---");
-
+myScreen.set('*' );
+  for(auto i=0u; i<5; i++){  
+    myScreen.forward();
+    myScreen.set('*' );
+   }
+   myScreen.move(1,4);
+   for(auto i=0u;i<5; i++)
+    {
+      myScreen.down();
+       myScreen.set('*');
+    }
 	myScreen.display();
 	cout << endl;
 
-	myScreen.reSize(16,16);
-	myScreen.display();
-	myScreen.clear(' ');
 
-	myScreen.move(7,7);
-	myScreen.set("BIG");
-	myScreen.move(8,5);
-	myScreen.set("SCREEN");
-	myScreen.display();
 
-	return 0;
 }
 
